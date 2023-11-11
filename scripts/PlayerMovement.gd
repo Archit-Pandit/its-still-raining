de635a0b5f326b5a73933a4acd29ceaaa8799021
Archit Pandit <archit.n.pandit90@gmail.com>
2023-11-10 22:12:41 -0500

    Added walk and idle animations to player.

--- scripts/PlayerMovement.gd
+++ scripts/PlayerMovement.gd
@@ -5,6 +5,8 @@ const ACCEL = 1000.0
 const FRIC = 1000.0
 const JUMP_VELOCITY = -250.0
 
+@onready var animSprite = $AnimatedSprite2D
+
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
@@ -16,11 +18,10 @@ func _physics_process(delta):
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var inputDir = Input.get_axis("ui_left", "ui_right")
 
-	#if x is a shortcut for if x != 0 when x is a number
-	if inputDir:
-		velocity.x = move_toward(velocity.x, SPEED * inputDir, ACCEL * delta)
-
+	ApplyAcceleration(inputDir, delta)
 	ApplyFriction(inputDir, delta)
+
+	HandleAnim(inputDir)
 
 	move_and_slide()
 
@@ -36,7 +37,18 @@ func HandleJump():
 		if Input.is_action_just_released("ui_accept") and velocity.y <= JUMP_VELOCITY / 2:
 			velocity.y = JUMP_VELOCITY / 2
 
+func ApplyAcceleration(dir, delta):
+	#if x is a shortcut for if x != 0 when x is a number
+	if dir:
+		velocity.x = move_toward(velocity.x, SPEED * dir, ACCEL * delta)
 
 func ApplyFriction(direction, delta):
 	if direction == 0:
 		velocity.x = move_toward(velocity.x, 0, FRIC * delta)
+
+func HandleAnim(dir):
+	if dir:
+		animSprite.flip_h = (dir < 0)
+		animSprite.play("run")
+	else:
+		animSprite.play("idle")
