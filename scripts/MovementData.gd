243563342a41be52146e3f3c7caba1830aa524e5
Archit Pandit <archit.n.pandit90@gmail.com>
2023-11-12 11:02:02 -0500

    Added movement value .tres to player movement script.

---
+++ scripts/MovementData.gd
@@ -0,0 +1,7 @@
+class_name MovementData
+extends Resource
+
+@export var speed = 100.0
+@export var acceleration = 1000.0
+@export var friction = 1000.0
+@export var jumpVelocity = -250.0
--- scripts/PlayerMovement.gd
+++ scripts/PlayerMovement.gd
@@ -1,9 +1,6 @@
 extends CharacterBody2D
 
-const SPEED = 100.0
-const ACCEL = 1000.0
-const FRIC = 1000.0
-const JUMP_VELOCITY = -250.0
+@export var movementData : MovementData
 
 # Node References
 @onready var animSprite = $AnimatedSprite2D
@@ -32,19 +29,19 @@ func ApplyGrav(delta):
 func HandleJump():
 	if is_on_floor() or coyoteJumpTimer.time_left > 0.0:
 		if Input.is_action_just_pressed("jump"):
-			velocity.y = JUMP_VELOCITY
+			velocity.y = movementData.jumpVelocity
 	if not is_on_floor():
-		if Input.is_action_just_released("jump") and velocity.y <= JUMP_VELOCITY / 2:
-			velocity.y = JUMP_VELOCITY / 2
+		if Input.is_action_just_released("jump") and velocity.y <= movementData.jumpVelocity / 2:
+			velocity.y = movementData.jumpVelocity / 2
 
 func ApplyAcceleration(dir, delta):
 	#if x is a shortcut for if x != 0 when x is a number
 	if dir:
-		velocity.x = move_toward(velocity.x, SPEED * dir, ACCEL * delta)
+		velocity.x = move_toward(velocity.x, movementData.speed * dir, movementData.acceleration * delta)
 
 func ApplyFriction(direction, delta):
 	if direction == 0:
-		velocity.x = move_toward(velocity.x, 0, FRIC * delta)
+		velocity.x = move_toward(velocity.x, 0, movementData.friction * delta)
 
 func HandleAnim(dir):
 	if dir:
